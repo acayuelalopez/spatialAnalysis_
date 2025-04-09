d86295d06c961fa1a23d87fc4777aa5d8b09465b
acayuelalopez <83207172+acayuelalopez@users.noreply.github.com>
2025-04-09 13:11:54 +0200

    Update count_data_TCell_Inf_NO_Inf.py

--- count_data_TCell_Inf_NO_Inf.py
+++ count_data_TCell_Inf_NO_Inf.py
@@ -28,17 +28,13 @@ def process_files(input_directory, tumor_border_directory, output_directory, dat
                 status_index = split_parts.index('No')
                 status = 'No_Inf'
                 population = '_'.join(split_parts[:status_index])
-            elif 'Inf' in split_parts:
+            else:
                 status_index = split_parts.index('Inf')
                 status = 'Inf'
                 population = '_'.join(split_parts[:status_index])
-            else:
-                print(f"Unexpected format in filename: {filename}")
-                continue
 
             # Now population should be 'Macrophage M1' and status should be 'Inf' or 'No_Inf'
             print(f"Population: {population}, Status: {status}")
-
 
             # Determine condition and patient ID based on the condition
             if condition_patient.startswith("POST"):
@@ -60,11 +56,27 @@ def process_files(input_directory, tumor_border_directory, output_directory, dat
                 print(f"'Classification' column not found in {filename}")
                 continue
 
-            # Count the total number of cells
-            total_cells = len(df)
+            # Filter rows based on status for tumor distances
+            if status == 'Inf':
+                df_filtered = df[df['Parent'] == 'Annotation (Infiltrated)']
+            else:
+                df_filtered = df[df['Parent'] == 'Annotation (No_Infiltrated)']
 
+            # Count the total number of cells
+            total_cells = len(df_filtered)
+
+            # Find the maximum length of strings in the Classification column
+            max_length = df_filtered['Classification'].str.len().max()
+
+            # Filter the DataFrame to keep only rows with the longest strings in the Classification column
+            df_filtered = df_filtered[df_filtered['Classification'].str.len() == max_length]
+
+            # Now you can proceed with counting the number of cells for each classification
+            classification_counts = df_filtered['Classification'].value_counts()
+
+            print(classification_counts)
             # Count the number of cells for each classification
-            classification_counts = df['Classification'].value_counts()
+            # classification_counts = df_filtered['Classification'].value_counts()
 
             # Get the tumor and CK+ areas from the data file
             tumor_area = data_df[(data_df['PatientID'] == int(patient_id)) & (data_df['Condition'] == condition)][
@@ -74,7 +86,8 @@ def process_files(input_directory, tumor_border_directory, output_directory, dat
 
             # Find the corresponding tumor border file
             tumor_border_file = next((tb_file for tb_file in os.listdir(tumor_border_directory) if
-                                      tb_file.endswith(".csv") and patient_id in tb_file), None)
+                                      tb_file.endswith(".csv") and patient_id in tb_file and condition in tb_file),
+                                     None)
             if tumor_border_file is None:
                 print(f"No corresponding tumor border file found for {filename}")
                 continue
@@ -85,18 +98,20 @@ def process_files(input_directory, tumor_border_directory, output_directory, dat
             print(f"Tumor border data loaded from {tumor_border_path}")
 
             # Calculate minimum distances from each cell centroid to the tumor border
-            cell_coords = df[['Centroid X µm', 'Centroid Y µm']].values
+            cell_coords = df_filtered[['Centroid X µm', 'Centroid Y µm']].values
             tumor_coords = tumor_border[['Centroid X (µm)', 'Centroid Y (µm)']].values
             tree = cKDTree(tumor_coords)
             distances, _ = tree.query(cell_coords)
 
             # Add distance columns to DataFrame
-            df['Min Distance to Tumor'] = distances
+            df_filtered['Min Distance to Tumor'] = distances
+            # Filtrar distancias menores o iguales a 100 solo para el cálculo de distancias
+            df_filtered_tumor = df_filtered[df_filtered['Min Distance to Tumor'] <= 100]
 
             # Calculate average, min, and max distances to tumor border for each classification
-            avg_distance_to_tumor = df.groupby('Classification')['Min Distance to Tumor'].mean()
-            min_distance_to_tumor = df.groupby('Classification')['Min Distance to Tumor'].min()
-            max_distance_to_tumor = df.groupby('Classification')['Min Distance to Tumor'].max()
+            avg_distance_to_tumor = df_filtered_tumor.groupby('Classification')['Min Distance to Tumor'].mean()
+            min_distance_to_tumor = df_filtered_tumor.groupby('Classification')['Min Distance to Tumor'].min()
+            max_distance_to_tumor = df_filtered_tumor.groupby('Classification')['Min Distance to Tumor'].max()
 
             # Store the quantification results in the dictionary
             if (population, patient_id) not in quantification_results:
@@ -125,45 +140,39 @@ def process_files(input_directory, tumor_border_directory, output_directory, dat
                 })
 
             # Find the maximum number of elements separated by ':'
-            max_elements_count = df['Classification'].dropna().apply(lambda x: len(x.split(':'))).max()
+            max_elements_count = df_filtered['Classification'].dropna().apply(lambda x: len(x.split(':'))).max()
 
             # Remove values in Classification column that do not contain the maximum number of elements separated by ':'
-            df.loc[df['Classification'].apply(lambda x: len(x.split(':')) != max_elements_count if isinstance(x,
-                                                                                                              str) else False), 'Classification'] = ''
+            df_filtered.loc[
+                df_filtered['Classification'].apply(lambda x: len(x.split(':')) != max_elements_count if isinstance(x,
+                                                                                                                    str) else False), 'Classification'] = ''
 
             # Save the output data with distances to a new CSV file
             output_csv_file = os.path.join(output_directory,
                                            f'{condition}_{patient_id}_{population}_{status}_tumor_distances.csv')
-            df.to_csv(output_csv_file, index=False)
+            df_filtered.to_csv(output_csv_file, index=False)
             print(f"Saved combined data with distances to {output_csv_file}")
 
     # Process TCell files separately and update quantification results with TCell distances
     for tcell_filename in os.listdir(input_directory):
         if "TCell" in tcell_filename and tcell_filename.endswith(".csv"):
-            print(tcell_filename+"--------")
+            print(tcell_filename + "--------")
             # Extract condition, patient ID, and population from the filename
             parts_tcell = tcell_filename.split('.ome.tif')
             condition_patient_tcell = parts_tcell[0]
             population_status = parts_tcell[1].replace('.csv', '')
-
             # Split population_status into population and status
             split_parts = population_status.split('_')
             if 'No' in split_parts:
                 status_index = split_parts.index('No')
                 status = 'No_Inf'
                 population = '_'.join(split_parts[:status_index])
-            elif 'Inf' in split_parts:
+            else:
                 status_index = split_parts.index('Inf')
                 status = 'Inf'
                 population = '_'.join(split_parts[:status_index])
-            else:
-                print(f"Unexpected format in filename: {tcell_filename}")
-                continue
-
             # Now population should be 'Macrophage M1' and status should be 'Inf' or 'No_Inf'
             print(f"Population: {population}, Status: {status}")
-
-
             # Determine condition and patient ID based on the condition
             if condition_patient_tcell.startswith("POST"):
                 condition_tcell = condition_patient_tcell[:4]
@@ -171,32 +180,24 @@ def process_files(input_directory, tumor_border_directory, output_directory, dat
             else:  # Assuming the other condition is PRE
                 condition_tcell = condition_patient_tcell[:3]
                 patient_id_tcell = condition_patient_tcell[3:]
-
             # Read the TCell CSV file
             tcell_file_path = os.path.join(input_directory, tcell_filename)
             df_tcell = pd.read_csv(tcell_file_path, sep='\t')
-
             # Print column names to debug
             print(f"Columns in {tcell_filename}: {df_tcell.columns.tolist()}")
-
             # Check if 'Classification' column exists and contains TCell label
-            #if 'Classification' not in df_tcell.columns or "CD3+: CD16-: CD163-: CK-: HLA-DR-" not in df_tcell['Classification'].values:
-            if 'Classification' not in df_tcell.columns or "CD3+: CD16-: CD206-: CD163-" not in df_tcell[
+            if 'Classification' not in df_tcell.columns or "CD3+: CD16-: CD163-: HLA-DR-" not in df_tcell[
                 'Classification'].values:
                 print(f"'Classification' column not found or TCell label not found in {tcell_filename}")
                 continue
-
             # Filter cells with TCell classification
-            #tcell_cells = df_tcell[df_tcell['Classification'] == "CD3+: CD16-: CD163-: CK-: HLA-DR-"]
-            tcell_cells = df_tcell[df_tcell['Classification'] == "CD3+: CD16-: CD206-: CD163-"]
-
+            tcell_cells = df_tcell[df_tcell['Classification'] == "CD3+: CD16-: CD163-: HLA-DR-"]
             # Calculate minimum distances from each cell centroid to the TCell cells for each population file again
             for filename in os.listdir(input_directory):
-                if filename.endswith(".csv"): #and "TCell" not in filename:
+                if filename.endswith(".csv"):
                     parts_pop = filename.split('.ome.tif')
                     condition_patient_pop = parts_pop[0]
                     population_status_pop = parts_pop[1].replace('.csv', '')
-
                     # Split population_status into population and status
                     split_parts = population_status_pop.split('_')
                     if 'No' in split_parts:
@@ -204,12 +205,9 @@ def process_files(input_directory, tumor_border_directory, output_directory, dat
                         status_pop = 'No_Inf'
                         population_pop = '_'.join(split_parts[:status_index])
                     else:
-                        print(f"Unexpected format in filename: {tcell_filename}")
-                        continue
-
-                    # Now population should be 'Macrophage M1' and status should be 'No_Inf'
-                    print(f"Population: {population}, Status: {status_pop}")
-
+                        status_index = split_parts.index('Inf')
+                        status_pop = 'Inf'
+                        population_pop = '_'.join(split_parts[:status_index])
                     if condition_patient_pop.startswith("POST"):
                         condition_pop = condition_patient_pop[:4]
                         patient_id_pop = condition_patient_pop[4:]
@@ -217,47 +215,57 @@ def process_files(input_directory, tumor_border_directory, output_directory, dat
                         condition_pop = condition_patient_pop[:3]
                         patient_id_pop = condition_patient_pop[3:]
 
+
                     # Check if the condition and patient ID match
                     if condition_pop == condition_tcell and patient_id_pop == patient_id_tcell:
+                        print(patient_id_pop)
+                        print(patient_id_tcell)
                         pop_file_path = os.path.join(input_directory, filename)
                         df_pop = pd.read_csv(pop_file_path, sep='\t')
-                        cell_coords_pop = df_pop[['Centroid X µm', 'Centroid Y µm']].values
+                        # Filter rows based on status for tumor distances
+                        if status_pop == 'Inf':
+                            df_pop_filtered = df_pop[df_pop['Parent'] == 'Annotation (Infiltrated)']
+                        else:
+                            df_pop_filtered = df_pop[df_pop['Parent'] == 'Annotation (No_Infiltrated)']
+                        cell_coords_pop = df_pop_filtered[['Centroid X µm', 'Centroid Y µm']].values
                         tcell_coords = tcell_cells[['Centroid X µm', 'Centroid Y µm']].values
                         tree_tcell = cKDTree(tcell_coords)
                         distances_tcell, _ = tree_tcell.query(cell_coords_pop)
-
                         # Add distance columns to DataFrame for TCell cells
-                        df_pop['Min Distance to TCell'] = distances_tcell
-
+                        df_pop_filtered['Min Distance to TCell'] = distances_tcell
+                        # Filter distances less than or equal to 100 for distance calculations
+                        df_pop_filtered_tcell = df_pop_filtered[df_pop_filtered['Min Distance to TCell'] <= 100]
+                        # Find the maximum length of strings in the Classification column
+                        max_length = df_pop_filtered_tcell['Classification'].str.len().max()
+                        # Filter the DataFrame to keep only rows with the longest strings in the Classification column
+                        df_pop_filtered_tcell = df_pop_filtered_tcell[
+                            df_pop_filtered_tcell['Classification'].str.len() == max_length]
                         # Calculate average, min, and max distances to TCell cells for each classification
-                        avg_distance_to_tcell = df_pop.groupby('Classification')['Min Distance to TCell'].mean()
-                        min_distance_to_tcell = df_pop.groupby('Classification')['Min Distance to TCell'].min()
-                        max_distance_to_tcell = df_pop.groupby('Classification')['Min Distance to TCell'].max()
-
-                        # Find the maximum number of elements separated by ':'
-                        max_elements_count = df_pop['Classification'].dropna().apply(lambda x: len(x.split(':'))).max()
-
-                        # Remove values in Classification column that do not contain the maximum number of elements separated by ':'
-                        df_pop.loc[df_pop['Classification'].apply(
-                            lambda x: len(x.split(':')) != max_elements_count if isinstance(x,
-                                                                                            str) else False), 'Classification'] = ''
-
+                        avg_distance_to_tcell = df_pop_filtered_tcell.groupby('Classification')[
+                            'Min Distance to TCell'].mean()
+                        min_distance_to_tcell = df_pop_filtered_tcell.groupby('Classification')[
+                            'Min Distance to TCell'].min()
+                        max_distance_to_tcell = df_pop_filtered_tcell.groupby('Classification')[
+                            'Min Distance to TCell'].max()
                         # Save the output data with TCell distances to a new CSV file
+
                         output_csv_file_tcell = os.path.join(output_directory,
                                                              f'{condition_pop}_{patient_id_pop}_{population_pop}_{status_pop}_TCell_distances.csv')
-                        df_pop.to_csv(output_csv_file_tcell, index=False)
+                        df_pop_filtered_tcell.to_csv(output_csv_file_tcell, index=False)
                         print(f"Saved TCell data with distances to {output_csv_file_tcell}")
-
                         # Update quantification results with TCell distances for each classification count entry
                         if (population_pop, patient_id_pop) in quantification_results:
                             for result in quantification_results[(population_pop, patient_id_pop)]:
-                                if result['Condition'] == condition_pop and result[
+                                if result['Condition'] == condition_pop and result['Status'] == status_pop and result[
                                     'Classification'] in avg_distance_to_tcell.index:
                                     result.update({
                                         'Average Distance to TCell': avg_distance_to_tcell[result['Classification']],
                                         'Min Distance to TCell': min_distance_to_tcell[result['Classification']],
                                         'Max Distance to TCell': max_distance_to_tcell[result['Classification']]
                                     })
+
+                            print(
+                                f"Updated quantification results for Population: {population_pop}, PatientID: {patient_id_pop}, Classification: {result['Classification']}")
                         else:
                             print(f"Key ({population_pop}, {patient_id_pop}) not found in quantification_results")
     # Save the updated quantification results to CSV files
@@ -289,11 +297,12 @@ def process_files(input_directory, tumor_border_directory, output_directory, dat
         output_path = os.path.join(output_directory, output_filename)
         combined_df_filtered.to_csv(output_path, index=False)
 
+
 # Example usage
-input_directory = "/mnt/imgserver/CONFOCAL/IA/Projects/2025/2025_2_28_egonzalez/test/input_test"
-tumor_border_directory = "/mnt/imgserver/CONFOCAL/IA/Projects/2024/2024_01_29_egonzalez/data/eva-gonzalez/spacio1/output/geojson"
-#output_directory = "/mnt/imgserver/CONFOCAL/IA/Projects/2024/2024_01_29_egonzalez/test/csv/csv"
-output_directory = "/mnt/imgserver/CONFOCAL/IA/Projects/2025/2025_2_28_egonzalez/test/output_test"
+input_directory = "/mnt/imgserver/CONFOCAL/IA/Projects/2024/2024_01_29_egonzalez/data/eva-gonzalez/spacio1/new_analysis_def"
+tumor_border_directory = "/mnt/imgserver/CONFOCAL/IA/Projects/2024/2024_01_29_egonzalez/data/eva-gonzalez/spacio1/output/tumor_masks"
+output_directory = "/home/acayuela/Desktop/output_test"
+# output_directory = "/mnt/imgserver/CONFOCAL/IA/Projects/2025/2025_2_28_egonzalez/test/output_def"
 data_file = "/mnt/imgserver/CONFOCAL/IA/Projects/2024/2024_01_29_egonzalez/count/csv/tumor_anot_area.ods"
 
 process_files(input_directory, tumor_border_directory, output_directory, data_file)
